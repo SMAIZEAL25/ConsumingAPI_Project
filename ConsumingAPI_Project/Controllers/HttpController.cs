c9587f8e827246e0efd8949d3453db3fff63c7de
smaizeal25 <samizeal25@gmail.com>
2025-02-15 11:57:49 +0100

    modification

--- ConsumingAPI_Project/Controllers/HttpController.cs
+++ ConsumingAPI_Project/Controllers/HttpController.cs
@@ -1,10 +1,7 @@
 ﻿using ConsumingAPI_Project.Model;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
-using System.Security.Cryptography.X509Certificates;
 using System.Text;
-
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace ConsumingAPI_Project.Controllers
 {
@@ -13,11 +10,13 @@ namespace ConsumingAPI_Project.Controllers
     public class HttpController : ControllerBase
     {
         private readonly IConfiguration _configuration;
+        private readonly string _baseUrl;
+
         public HttpController(IConfiguration configuration)
         {
-            this._configuration = configuration;
+            _configuration = configuration;
+            _baseUrl = _configuration["baseurl"] ?? throw new ArgumentNullException("Base URL not configured");
         }
-
 
         // GET: api/<HttpController>
         [HttpGet]
@@ -27,24 +26,23 @@ namespace ConsumingAPI_Project.Controllers
             List<APIObject> apiObjects = new List<APIObject>();
 
             var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync($"{_baseUrl}/Object");
+            {
+                string apiResponse = await response.Content.ReadAsStringAsync();
+                apiObjects = JsonConvert.DeserializeObject<List<APIObject>>(apiResponse) 
+                    ?? new List<APIObject>();
+            }
 
-               var response = await httpClient.GetAsync("https://restful-api.dev/Object");
-                {
-                    string apiResponse = await response.Content.ReadAsStringAsync();
-                    apiObjects = JsonConvert.DeserializeObject<List<APIObject>>(apiResponse);
-                }
-            
             return apiObjects;
         }
 
         // GET api/<HttpController>/5
         [HttpGet("{id}Get/requestById")]
-
-        public async Task<APIObject> GetObjectIdAsync(string Id)
+        public async Task<APIObject?> GetObjectIdAsync(string Id)
         {
-            APIObject aPIObject = new APIObject();
+            APIObject? aPIObject = null;
             var httpClient = new HttpClient();
-            var response = await httpClient.GetAsync("https://api.restful-api.dev/objects?id=3&id=5&id=10" + Id);
+            var response = await httpClient.GetAsync($"{_baseUrl}/objects?id=3&id=5&id=10" + Id);
             {
                 string apiResponse = await response.Content.ReadAsStringAsync();
                 aPIObject = JsonConvert.DeserializeObject<APIObject>(apiResponse);
@@ -52,56 +50,53 @@ namespace ConsumingAPI_Project.Controllers
             return aPIObject;
         }
 
-
-            // POST api/<HttpController>
+        // POST api/<HttpController>
         [HttpPost]
-        public async Task<APIObject> CreatedObjectAsync (CreateAPIObjectRequest request)
-
+        public async Task<APIObject?> CreatedObjectAsync(CreateAPIObjectRequest request)
         {
-            APIObject aPIObject = new APIObject();
+            APIObject? aPIObject = null;
             var httpClient = new HttpClient();
             var json = JsonConvert.SerializeObject(request);
 
-            var content = new StringContent(json, Encoding.UTF8, "https://api.restful-api.dev/objects");
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
 
-            var response = await httpClient.PostAsync("https://api.restful-api.dev/objects", content);
+            var response = await httpClient.PostAsync($"{_baseUrl}/objects", content);
 
             response.EnsureSuccessStatusCode();
 
             var responseJson = await response.Content.ReadAsStringAsync();
 
-            return JsonConvert.DeserializeObject<APIObject>(responseJson);
+            aPIObject = JsonConvert.DeserializeObject<APIObject>(responseJson);
+            return aPIObject;
         }
-
 
         // PUT api/<HttpController>/5
         [HttpPut("{id}")]
-        public async Task<APIObject> UpdateObjectAsync(string id, UpdateAPIObject updateAPIObject)
+        public async Task<APIObject?> UpdateObjectAsync(string id, UpdateAPIObject updateAPIObject)
         {
             var httpClient = new HttpClient();
             var json = JsonConvert.SerializeObject(updateAPIObject);
 
-            var content = new StringContent(json, Encoding.UTF8, "https://api.restful-api.dev/objects");
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
 
-            var response = await httpClient.PutAsync($"https://api.restful-api.dev/objects/{id}", content);
+            var response = await httpClient.PutAsync($"{_baseUrl}/objects/{id}", content);
             response.EnsureSuccessStatusCode();
 
             var responseJson = await response.Content.ReadAsStringAsync();
             return JsonConvert.DeserializeObject<APIObject>(responseJson);
         }
 
-
         // Partially Update the record 
         [HttpPatch("{Id}")]
-        public async Task<APIObject> PartiallyUpdateObjectAync (string Id, PartialUpdateApiObjectRequest partialUpdateApi)
+        public async Task<APIObject?> PartiallyUpdateObjectAync(string Id, PartialUpdateApiObjectRequest partialUpdateApi)
         {
             var httpClient = new HttpClient();
 
             var json = JsonConvert.SerializeObject(partialUpdateApi);
 
-            var content = new StringContent(json, Encoding.UTF8, "");
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
 
-            var response = await httpClient.PatchAsync($"https://api.restful-api.dev/objects/{Id}", content);
+            var response = await httpClient.PatchAsync($"{_baseUrl}/objects/{Id}", content);
 
             response.EnsureSuccessStatusCode();
 
@@ -110,14 +105,12 @@ namespace ConsumingAPI_Project.Controllers
             return JsonConvert.DeserializeObject<APIObject>(responseJson);
         }
 
-
         // DELETE api/<HttpController>/5
         [HttpDelete("{id}")]
-
-        public async Task<DeleteApiObjectResponse> DeleteApiObjectAsync (string id)
+        public async Task<DeleteApiObjectResponse?> DeleteApiObjectAsync(string id)
         {
             var httpClient = new HttpClient();
-            var response = await httpClient.DeleteAsync($"https://api.restful-api.dev/objects/ {id}");
+            var response = await httpClient.DeleteAsync($"{_baseUrl}/objects/{id}");
 
             response.EnsureSuccessStatusCode();
 
--- ConsumingAPI_Project/Program.cs
+++ ConsumingAPI_Project/Program.cs
@@ -1,19 +1,27 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+});
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
-   
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    });
 }
 
 app.UseHttpsRedirection();
